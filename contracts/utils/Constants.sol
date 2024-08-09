6aba7c6be012c4be1305184e5e988d30634ec126
OswaldQiwys <HaylockEhrlich@gmail.com>
2024-08-10 07:44:00 +0800

    feat: add constants library for contract parameters

---
+++ contracts/utils/Constants.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/**
+ * @title Constants
+ * @notice Global constants used across contracts
+ */
+library Constants {
+    uint256 constant MIN_INTERVAL = 1 days;
+    uint256 constant MAX_INTERVAL = 365 days;
+    uint256 constant MIN_AMOUNT = 1e6; // 1 USDC (6 decimals)
+    uint256 constant PERCENTAGE_BASE = 100;
+    uint256 constant DEFAULT_REFUND_WINDOW = 7 days;
+}
+
