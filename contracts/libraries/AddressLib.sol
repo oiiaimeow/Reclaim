03ae954273ea94da879ad8478041ff5fb7822225
oiiaimeow <tilliemarkin@gmail.com>
2024-12-07 19:24:00 +0800

    feat: add AddressLib utility library

---
+++ contracts/libraries/AddressLib.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+library AddressLib {
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Insufficient balance");
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "Transfer failed");
+    }
+}
+
